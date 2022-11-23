5aecc6d12a9a7858a73b920112300b394a446415
bannovdaniil <baddan@mail.ru>
2022-11-23 19:56:53 +0300

    !12 task: abstract;

--- 13_abstract_classes/src/Task1abs.kt
+++ 13_abstract_classes/src/Task1abs.kt
@@ -5,3 +5,61 @@
 
 Для этой задачи есть видео с разбором.
 */
+
+fun main() {
+    val round = RoundZ(10, "red");
+    val rectangle = RectangleZ(4, 5, "Yellow");
+
+    println(round.color);
+    println(rectangle.isSquare());
+}
+
+abstract class FigureZ(val color: String) {
+    var x: Int = 0;
+    var y: Int = 0;
+
+    abstract fun area(): Double;
+
+    abstract fun perimeter(): Double;
+
+    fun moveLeft(stepX: Int) {
+        x -= stepX;
+    }
+
+    fun moveRight(stepX: Int) {
+        x += stepX;
+    }
+
+    fun moveDown(stepY: Int) {
+        y += stepY;
+    }
+
+    fun moveUp(stepY: Int) {
+        y -= stepY;
+    }
+}
+
+class RectangleZ(private val width: Int, private val height: Int, color: String) : FigureZ(color) {
+    override fun area(): Double {
+        return width * height * 1.0;
+    }
+
+    override fun perimeter(): Double {
+        return 2.0 * (width + height);
+    }
+
+    fun isSquare(): Boolean {
+        return width == height;
+    }
+
+}
+
+class RoundZ(val radius: Int, color: String) : FigureZ(color) {
+    override fun area(): Double {
+        return Math.PI * radius * radius;
+    }
+
+    override fun perimeter(): Double {
+        return Math.PI * radius;
+    }
+}
--- 13_abstract_classes/src/Task2abs.kt
+++ 13_abstract_classes/src/Task2abs.kt
@@ -1,6 +1,32 @@
+import kotlin.math.sqrt
+
 /*
 Добавить еще один класс Triangle – тоже наследник Figure (который вы сделали в предыдущем задании)
 и прописать работу функций area() и length() для него
 
 Для этой задачи есть видео с разбором.
 */
+
+fun main() {
+    val tr1 = TriangleZ(3, 4, 5, "red");
+
+    println(tr1.perimeter());
+    println(tr1.area());
+}
+
+class TriangleZ(
+    private val sideA: Int,
+    private val sideB: Int,
+    private val sideC: Int,
+    color: String
+) : FigureZ(color) {
+    override fun area(): Double {
+        val per = perimeter() / 2.0;
+        return sqrt(per * (per - sideA) * (per - sideB) * (per - sideC));
+    }
+
+    override fun perimeter(): Double {
+        return (sideA + sideB + sideC).toDouble();
+    }
+
+}
